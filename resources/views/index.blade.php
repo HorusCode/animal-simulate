c41487b4eba130288fb5378997a3e2ae6e12a35c
horuscode <horusm99@gmail.com>
2022-12-06 19:42:04 +0300

    Configure vite for vue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
-
+    @vite('resources/css/app.css')
 </head>
 <body>
-<div id="app">
-
-</div>
+<div id="app"></div>
+@vite('resources/js/app.js')
 </body>
 </html>
